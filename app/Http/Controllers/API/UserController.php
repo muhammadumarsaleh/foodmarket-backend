9bb794250185a0598e59f7b7b6c823232d472001
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-09-27 09:14:55 +0800

    membuat API user, API upload photo, API food, and API transaksi

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Actions\Fortify\PasswordValidationRules;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,6 +9,8 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Actions\Fortify\PasswordValidationRules;
 
 class UserController extends Controller
 {
@@ -110,6 +111,26 @@ class UserController extends Controller
         return ResponseFormatter::success($user, 'Profile Updated');
     }
 
+    public function updatePhoto(Request $request){
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048',
+        ]);
+
+        if($request->file('file'))
+        {
+            $file = $request->file->store('assets/user', 'public');
+
+            // simpan foto ke database (urlnya)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File successfully uploaded');
+        }
+    }
+    
+
 
 
 }
+  
